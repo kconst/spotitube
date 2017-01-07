1aaa6d48f71498d8389c37e1e57d1ecabfd23e87
Kyle Constance <kconst@gmail.com>
2017-01-07 02:56:41 +0000

    added snapshots and tests

--- src/components/Tile.test.js
+++ src/components/Tile.test.js
@@ -1,16 +1,22 @@
 import React from 'react';
-// import Tile from './Tile';
-// import renderer from 'react-test-renderer';
-// import {shallow} from 'enzyme';
+import Tile from './Tile';
+import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
-test('basic rendering', () => {
-  /*const jsx = <Tile image="http://iscale.iheart.com/catalog/artist/58269?ops=fit(250,0)" name="Metallica" description="they a band"></Tile>,
-    //   component = renderer.create(jsx),
-      tile = shallow(jsx);
-      
-      // snapshot check
-    // let tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
+test('snapshot comparison', () => {
+  const jsx = <Tile images={ [{ url: 'https://i.scdn.co/image/5194f3860bd53aa613edcfc5ef09a995b599f049' }] } name="Trivium"></Tile>,
+    component = renderer.create(jsx),
+    tile = component.toJSON();
+  
+  // snapshot check
+  expect(tile).toMatchSnapshot();
+});
 
-    expect(tile.find('h2').text()).toEqual('Metallica');*/
+test('it should have an image and a title', () => {
+  const jsx = <Tile images={ [{ url: 'https://i.scdn.co/image/5194f3860bd53aa613edcfc5ef09a995b599f049' }] } name="Trivium"></Tile>,
+    tile = shallow(jsx);
+  
+  expect(tile.find('h2').text()).toEqual('Trivium');
+  
+  expect(tile.find('img').length).toEqual(1);
 });
